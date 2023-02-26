5a717797c7e2164fed1c1e07a39699b61fd4b3ca
Olga Shaban <117861745+olga-shaban@users.noreply.github.com>
2023-02-26 15:19:57 +0300

    feat: update task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+  const OPEN_BRACKETS = ['(', '{'];
+  const BRACKETS_PAIR = {
+    [')']: '(',
+    ['}']: '{',
+  };
+
+  function isBracketsOk(str) {
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      
+      if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
 }
